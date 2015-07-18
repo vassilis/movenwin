f257fbd36ce4c01edcbdf3a3e98c3e93ce282cb2
Konstantina Giapoutzi <gkonstantina18@gmail.com>
2015-07-18 22:30:52 +0300

    changed admin after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
 		if resource.is_a?(Admin)
-			admin_dashboard_path
+			admin_path
 		else
 			root_path
 		end

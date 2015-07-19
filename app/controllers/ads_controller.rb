85655374f3986bfaa24d3023f9de2ae2f15ce191
Konstantina Giapoutzi <gkonstantina18@gmail.com>
2015-07-19 14:12:24 +0300

    changed redirect after adding points

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -13,7 +13,7 @@ class AdsController < ApplicationController
     respond_to do |format|
       current_user.add_points(points)
       current_user.save
-      format.html { redirect_to ads_index_path, notice: 'Οι πόντοι σας καταχωρήθηκαν επιτυχώς.' }
+      format.html { redirect_to root_path, notice: 'Οι πόντοι σας καταχωρήθηκαν επιτυχώς.' }
       format.json { render :show, status: :ok, location: @user }
     end
   end

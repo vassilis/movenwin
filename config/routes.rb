417c8cde089916cadf60a50f85915c9e7cb9d93a
Dimitris Dovinos <dimitris.dovinos@gmail.com>
2015-07-19 12:35:29 +0300

    add user controller and functionality for inserting ticket

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  get '/users/scan_ticket'
+  put '/users/add_points'
+  put '/users/remove_points'
   get 'welcome/index'
 
   root 'welcome#index'
